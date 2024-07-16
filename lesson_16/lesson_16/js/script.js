cc2ba762644913ae37a57ea2e07e119accea4e35
Illia <anolij1@gmail.com>
2024-07-16 22:03:00 +0200

    dynamic-adaptive

--- lesson_16/lesson_16/js/script.js
+++ lesson_16/lesson_16/js/script.js
@@ -1,8 +1,6 @@
-
-
 document.addEventListener('click', function (e) {
-	const targetElement = e.target;
-	if (targetElement.closest('.menu-icon')) {
-		document.documentElement.classList.toggle('menu-open');
-	}
-})
+  const targetElement = e.target;
+  if (targetElement.closest('.menu-icon')) {
+    document.documentElement.classList.toggle('menu-open');
+  }
+});
